5d4ee6b96f1c8993c420e3d15d4dae99317effc0
dudukapolar8-rgb <dudukapolar8@gmail.com>
2026-01-31 23:41:19 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-gray-100 text-gray-900 font-sans">{children}</body>
     </html>
   );
 }
